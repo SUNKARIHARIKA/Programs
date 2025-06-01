0bfd470a5479800e36d91a30fe690192f195c9a7
SUNKARIHARIKA <140961125+SUNKARIHARIKA@users.noreply.github.com>
2025-06-01 12:41:01 +0530

    Update 3sum smaller.py

--- 3sum smaller.py
+++ 3sum smaller.py
@@ -2,17 +2,15 @@ nums = [-1, 0, 2, 3]
 target = 3
 nums.sort()
 result=[]
+count=0
 for i in range(len(nums)):
     j,k=i+1,len(nums)-1
     while j<k:
         s1=nums[i]+nums[j]+nums[k]
-        if s1>target:
-            k-=1
-        elif s1==target:
-            k-=1
-        else:
-            result.append([nums[i],nums[j],nums[k]])
+        if s1<target:
+            count+=(k-j)
             j+=1
+        else:
             k-=1
 print(result)
 #the time complexity is o(n**2)
