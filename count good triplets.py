2bb9298c3b27d7faaa6bd8a734745e11a5fba919
SUNKARIHARIKA <140961125+SUNKARIHARIKA@users.noreply.github.com>
2025-06-01 07:53:22 +0530

    Update count good triplets.py

--- count good triplets.py
+++ count good triplets.py
@@ -5,10 +5,12 @@ c = 3
 count=0
 for i in range(len(arr)):
     for j in range(i+1,len(arr)):
-        if abs(arr[i]-arr[j])<=a:
-            for k in range(j+1,len(arr)):
-                if abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c:
-                    count+=1
+        if abs(arr[i]-arr[j])>a:
+            continue
+        for k in range(j+1,len(arr)):
+            if abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c:
+                count+=1
 print(count)
+#when the value is greater than a then it will skip that iterationb without running the next k loop
 #the time complexity is o(n**3)
 #the space complexity is o(1)
