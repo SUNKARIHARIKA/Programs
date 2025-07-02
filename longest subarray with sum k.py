74cd6e03bdabebfceacc88824a72f13c6723cbc2
SUNKARIHARIKA <140961125+SUNKARIHARIKA@users.noreply.github.com>
2025-07-02 07:31:58 +0530

    Update longest subarray with sum k.py

--- longest subarray with sum k.py
+++ longest subarray with sum k.py
@@ -12,7 +12,7 @@ for i in range(len(arr)):
 print(longest)
 #the time complexity of the above approach is=>o(n**3)
 #the space complexity is o(1)
-#<----better approach---->
+#<----brute(optimal)---->
 longest=0
 for i in range(len(arr)):
     current_sum=0
@@ -23,7 +23,24 @@ for i in range(len(arr)):
 print(longest)
 #the time complexity is o(n**2)
 #the space complexity is o(1)
+#better solution
+arr=[1,2,3,1,1,1,1,3,3]
+k=6
+prefix_sum=0
+hash_map={}
+max_len=0
+for i in range(len(arr)):
+    prefix_sum=prefix_sum+arr[i]
+    if prefix_sum==k:
+        max_len=i
+    elif prefix_sum-k in hash_map:
+        length=i-hash_map[prefix_sum-k]
+        max_len=max(max_len,length)
+    elif prefix_sum-k not in hash_map:
+        hash_map[prefix_sum]=i
+print(max_len)
 #optimal approach
+#the sliding window approach works for only positive integer array
 arr=[1,2,3,1,1,1,1,3,3]
 k=6
 left,right=0,0
@@ -38,5 +55,8 @@ while right<len(arr):
     if current_sum==k:
         maxlen=max(maxlen,right-left)
 print(maxlen)
+    
+
+
     
 
